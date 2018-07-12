51bf69f36e58222cfe3321dd15eb83079748c45c
david811130 <david811130@gmail.com>
2018-07-12 08:26:38 +0200

    Stupid expression expected on line 64 in MainActivity.java

--- app/src/main/java/com/example/davidb/bridgestone_android/MainActivity.java
+++ app/src/main/java/com/example/davidb/bridgestone_android/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.davidb.bridgestone_android;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -11,7 +17,7 @@ import java.text.SimpleDateFormat;
 //import java.io.InputStreamReader;
 //import java.io.OutputStreamWriter;
 //import java.io.PrintWriter;
-//import java.net.InetAddress;
+//import java.net.Inetaddress;
 //import java.net.Socket;
 //import java.net.UnknownHostException;
 //import android.app.Activity;
@@ -22,10 +28,45 @@ import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView battery_text;
+    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            battery_text.setText(String.valueOf(level) + "%");
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+        final int[] batteryArray = {
+                R.drawable.slide1,
+                R.drawable.slide2,
+                R.drawable.slide3,
+                R.drawable.slide4,
+                R.drawable.slide5
+        };
+
+        battery_text = findViewById(R.id.textView3);
+        this.registerReceiver(this.mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+
+        if (battery_text = 100%){
+            ImageView batteryLevel = findViewById(R.id.imageView2);
+            batteryLevel.setImageResource(batteryArray[0]);
+        }
+
+
         Thread t = new Thread() {
             @Override
             public void run(){
@@ -51,5 +92,7 @@ public class MainActivity extends AppCompatActivity {
             };
         t.start();
         }
+
+
     }
 
